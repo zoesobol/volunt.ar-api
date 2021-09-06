481237958a86077dd2232a127913c538cbb2089a
zoesobol <zoesobol98@gmail.com>
2021-09-06 12:29:19 -0300

    register and user models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
+'''
 from django.shortcuts import render
+from rest_framework import generics
+from . import models
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class UserView(generics.ListAPIView):
+    queryset = models.User.objects.all()
+    serializer_class = UserSerializer
+'''
---
+++ users/views.py
@@ -0,0 +1,39 @@
+"""Users views."""
+
+# Django REST Framework
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+# Serializers
+from users.serializers import UserLoginSerializer, UserModelSerializer, UserSignUpSerializer
+
+# Models
+from users.models import User
+
+class UserViewSet(viewsets.GenericViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+
+    # Detail define si es una petición de detalle o no, en methods añadimos el método permitido, en nuestro caso solo vamos a permitir post
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        """User sign in."""
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user, token = serializer.save()
+        data = {
+            'user': UserModelSerializer(user).data,
+            'access_token': token
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['post'])
+    def signup(self, request):
+        """User sign up."""
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = UserModelSerializer(user).data
+        return Response(data, status=status.HTTP_201_CREATED)
